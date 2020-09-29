b5a4db6913162aa11aefc5d9ad2437f2df6d7201
Luciano Freitas <lucsilfreitas@gmail.com>
2020-09-29 14:25:59 -0300

    erro1

---
+++ deventoapp/src/main/java/com/eventoapp/deventoapp/DataConfiguration.java
@@ -0,0 +1,40 @@
+package com.eventoapp.deventoapp;
+
+import javax.activation.DataSource;
+
+import org.hibernate.Hibernate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	//cria 2 bean conexao com o bando e o hibernate
+	
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+		dataSource.setUsername("root");
+		dataSource.setPassword("0311");
+		return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+	
+	
+}
---
+++ deventoapp/src/test/java/com/eventoapp/deventoapp/DeventoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventoapp.deventoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeventoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
